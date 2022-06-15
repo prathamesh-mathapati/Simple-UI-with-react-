41bb79ff028c984b0b725473279f99f98c62ec66
Prathamesh Mathapati <93044026+prathamesh-mathapati@users.noreply.github.com>
2022-06-15 10:15:18 +0530

    Add files via upload

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,29 @@
+import React from "react"
+import ExpenseDate from "./ExpenseDate";
+
+import "./ExpenseItem.css"
+import Card from "./Card"
+
+const ExpenseItem = (props) =>{
+   
+    return ( 
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item_description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item_price">${props.amount}</div>
+                </div>
+            </Card>
+        </li>
+    );
+}
+
+export default ExpenseItem;
+
+
+
+
+
+
+
---
+++ src/components/ExpensesChart.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Chart from "./Chart/Chart";
+
+const ExpensesChart = props => {
+    const chartDataPoints = [
+        { lebel: 'Jan', value: 0 },
+        { lebel: 'Feb', value: 0 },
+        { lebel: 'Mar', value: 0 },
+        { lebel: 'Apr', value: 0 },
+        { lebel: 'May', value: 0 },
+        { lebel: 'Jun', value: 0 },
+        { lebel: 'Jul', value: 0 },
+        { lebel: 'Aug', value: 0 },
+        { lebel: 'Sep', value: 0 },
+        { lebel: 'Oct', value: 0 },
+        { lebel: 'Nov', value: 0 },
+        { lebel: 'Dec', value: 0 },
+    ];
+
+    for (const expense of props.expenses){
+        const expenseMonth = expense.date.getMonth();
+        chartDataPoints[expenseMonth].value += expense.amount;
+    }
+    return <Chart dataPoints={chartDataPoints}/>;
+}
+
+export default ExpensesChart;
